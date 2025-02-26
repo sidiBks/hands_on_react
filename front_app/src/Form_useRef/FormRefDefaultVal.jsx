8082ac96b87c60ad7e4070d6727e8844d6cd0dba
Ahmed Bks <lamghari_ahmed@outlook.fr>
2025-02-26 12:43:15 +0000

    Form with useRef

--- front_app/src/Form_useRef/FormRefDefaultVal.jsx
+++ front_app/src/Form_useRef/FormRefDefaultVal.jsx
@@ -7,27 +7,36 @@ export default function FormRef_DefaultVal() {
     const inputContryRef = useRef()
     const inputAcceptConditionsRef = useRef()
 
-    const [formValues, SetFormValues] = useState({
-        name: '',
-        country: 'MA',
-        age: null,
-        accept: false
-    })
+    const InitialNameValue = 'Test'
+    const InitialCountryValue = 'MA' 
+    const InitialAgeValue = 20
+    const InitialAcceptConditionsValue = true
 
-    useEffect(() => {
-        inputNameRef.current.value = 'Ahmed'
-        inputContryRef.current.value = 'MA'
-        inputAgeRef.current.value = 22
-        inputAcceptConditionsRef.current.checked = true
+    // useState changes affects initial values and changes formValues object
+    const [formValues, SetFormValues] = useState({
+        name: InitialNameValue,
+        country: InitialCountryValue,
+        age: InitialAgeValue,
+        accept: InitialAcceptConditionsValue
     }, [])
+
+    // useEffect shows the initial values in the render
+    useEffect(() => {
+        inputNameRef.current.value = InitialNameValue
+        inputContryRef.current.value = InitialCountryValue
+        inputAgeRef.current.value = InitialAgeValue
+        inputAcceptConditionsRef.current.checked = InitialAcceptConditionsValue
+    })
 
     const onSubmit = (event) => {
         event.preventDefault()
+
         const values = {
             name: inputNameRef.current.value,
             country: inputContryRef.current.value,
-            age: inputAgeRef.current.value,
-            acceptConditoons: inputAcceptConditionsRef.current.checked
+            // form converts age to string, useEffect give it in the first place as a number
+            age: inputAgeRef.current.value * 1 , // the *1 reconverts age to number
+            accept: inputAcceptConditionsRef.current.checked
         }        
 
         // rerender the app to update and display formValue
