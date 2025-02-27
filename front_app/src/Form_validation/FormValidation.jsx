781cd4e17dec657e421316b176da2fac97ff9dcd
Ahmed Bks <lamghari_ahmed@outlook.fr>
2025-02-27 12:20:46 +0000

    basic validation to FormValidation

--- front_app/src/Form_validation/FormValidation.jsx
+++ front_app/src/Form_validation/FormValidation.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 export default function FormValidation() {
@@ -8,17 +8,108 @@ export default function FormValidation() {
     const message = useRef()
     const acceptConditions = useRef()
 
-    const submitForm = (e) => {
-        e.preventDefault()
+    const [isFormSent, setIsFormSent] = useState(false)
+    const [errors, setErrors] = useState([])
+    // const [isFormValid, setIsFormValid] = useState(false)
+    let isFormValid = true
+
+    const validateForm = () => {
         const nameValue = name.current.value
         const emailValue = email.current.value
         const messageValue = message.current.value
         const acceptConditionsValue = acceptConditions.current.checked
-        console.log({nameValue, emailValue, messageValue, acceptConditionsValue})
+
+        setErrors([])
+
+        if (nameValue.trim() == '') {
+            // name.current.style.border = 'red 1px solid'
+            isFormValid = false
+            setErrors(prevState => {
+                return [...prevState, { field: 'name', message: ['field required'] }]
+            })
+        }
+
+        if (emailValue.trim() == '') {
+            // name.current.style.border = 'red 1px solid'
+            isFormValid = false
+            setErrors(prevState => {
+                return [...prevState, { field: 'email', message: ['field required'] }]
+            })
+        } else if (!emailValue.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+            isFormValid = false
+            setErrors(prevState => {
+                return [...prevState, { field: 'email', message: ['invalid email'] }]
+            })
+        }
+
+        if (messageValue.trim() == '') {
+            // name.current.style.border = 'red 1px solid'
+            isFormValid = false
+            setErrors(prevState => {
+                return [...prevState, { field: 'message', message: ['field required'] }]
+            })
+        }
+
+        if( !acceptConditionsValue ) {
+            isFormValid = false
+            setErrors(prevState => {
+                return [...prevState, { field: 'checkbox', message: ['field required'] }]
+            })
+        }
+
+        return isFormValid
+    }
+
+    // useEffect(() => {
+    //     setIsFormValid(errors.length == 0)
+    // }, [errors])
+
+    const displayErrors = () => {
+        return errors.map((error, key) => {
+            return <li key={key}> {error.field} : {error.message}</li>
+        })
+    }
+
+    const resetForm = () => {
+        name.current.value = ''
+        email.current.value = ''
+        message.current.value = ''
+        acceptConditions.current.checked = false
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        if (validateForm()) {
+            setIsFormSent(true)
+            resetForm()
+        }
+
     }
 
     return (
         <div className="container my-3">
+            {/* {isFormValid.toString()} */}
+
+            {
+                errors.length > 0 ?
+                    <div className="alert alert-danger" role="alert">
+                        <strong>ERRORS</strong>
+                        {/* {JSON.stringify(errors)} */}
+                        <ul>
+                            {displayErrors()}
+                        </ul>
+                    </div> : ''
+            }
+
+
+            {
+                isFormSent ?
+                    <div className="alert alert-success" role="alert">
+                        <strong>SUCCESS</strong> Message sent successfully !!
+                    </div> : ''
+            }
+
+
             <form onSubmit={submitForm}>
                 <h1>Contact Form</h1>
                 <hr />
